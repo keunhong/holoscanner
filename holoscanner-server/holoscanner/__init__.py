3323261bbbbce9124a21db1ac6f7d695c90e9864
Keunhong Park <kpar@cs.washington.edu>
2016-05-08 17:52:42 -0700

    Add loggging handler

--- holoscanner-server/holoscanner/__init__.py
+++ holoscanner-server/holoscanner/__init__.py
@@ -1,5 +1,11 @@
 import logging
+from holoscanner import config
 
+
+console = logging.StreamHandler()
+formatter = logging.Formatter(config.LOG_FORMAT)
+console.setFormatter(formatter)
 
 base_logger = logging.getLogger(__name__)
-
+base_logger.addHandler(console)
+base_logger.setLevel(config.LOG_LEVEL)
