b0e781f65a92dcef1b7e6d7ce3ec40ab5f718819
Edward Zhang <edward64@gmail.com>
2016-05-30 16:18:57 -0700

    Redo splash screen to allow for variable amount of text

--- holoscanner-client/Assets/Scripts/TitleScreenScript.cs
+++ holoscanner-client/Assets/Scripts/TitleScreenScript.cs
@@ -4,33 +4,61 @@ using System.Collections.Generic;
 
 public class TitleScreenScript : MonoBehaviour
 {
-    GameObject titletext, instructionstext, taptext;
+    GameObject titletext, instructionstext, waitingtext;
+    IEnumerator timingcoroutine;
+    string[] instructions =
+    {
+        "Achieve Zen by finding the orbs\nbefore the other players",
+        "Let sound guide you along your journey",
+        "Air tap orb to start game"
+    };
+    int instructionindex = 0;
     enum TitleState
     {
-        INITIAL,
-        INSTRUCTIONSIN,
-        INSTRUCTIONS,
-        INSTRUCTIONSOUTTRIGGER,
-        INSTRUCTIONSOUT,
-        TAPIN,
-        TAP
+        INITIAL,                   // Just started
+        SCREENIN,                  // fading in screen
+        SCREEN,                    // faded in screen
+        INSTRUCTIONSIN,            // fading in text for instructionindex
+        INSTRUCTIONS,              // faded in text, waiting
+        INSTRUCTIONSOUTTRIGGER,    // fading out text for instructionindex
+        INSTRUCTIONSOUT,           // done fading out text for instructionindex
+        WAITTRIGGERED,             // start final fadeout of text for waiting text
+        WAITOUT,                   // fading out text for waiting text
+        WAITIN,                    // fading in waiting text
+        WAIT,                      // faded in waiting text
+        GAMESTART,
     };
 
     IEnumerator timing()
     {
-        state = TitleState.INITIAL;
-        yield return new WaitForSeconds(5.0f);
-        state = TitleState.INSTRUCTIONSOUTTRIGGER;
+        for (; instructionindex < instructions.Length-1; instructionindex++)
+        {
+            yield return new WaitForSeconds(5.0f);
+            state = TitleState.INSTRUCTIONSOUTTRIGGER;
+        }
+    }
+    public void waitingForPlayers()
+    {
+        state = TitleState.WAITTRIGGERED;
     }
 
     public void gameStarted()
     {
+        state = TitleState.GAMESTART;
+        StopCoroutine(timingcoroutine);
         titletext.GetComponent<Fade>().startcolor = Color.black;
         instructionstext.GetComponent<Fade>().startcolor = Color.black;
-        taptext.GetComponent<Fade>().startcolor = Color.black;
+        waitingtext.GetComponent<Fade>().startcolor = Color.black;
+        titletext.GetComponent<Fade>().endcolor = titletext.GetComponent<Fade>().getCurrentColor();
+        instructionstext.GetComponent<Fade>().endcolor = instructionstext.GetComponent<Fade>().getCurrentColor();
+        waitingtext.GetComponent<Fade>().endcolor = waitingtext.GetComponent<Fade>().getCurrentColor();
+        titletext.GetComponent<Fade>().show(true);
+        instructionstext.GetComponent<Fade>().show(true);
+        waitingtext.GetComponent<Fade>().show(true);
         gameObject.GetComponent<Fade>().fadeOut();
         titletext.GetComponent<Fade>().fadeOut();
-        taptext.GetComponent<Fade>().fadeOut();
+        instructionstext.GetComponent<Fade>().fadeOut();
+        waitingtext.GetComponent<Fade>().fadeOut();
     }
 
     TitleState state;
@@ -40,7 +68,7 @@ public class TitleScreenScript : MonoBehaviour
         state = TitleState.INITIAL;
         titletext = GameObject.Find("TitleText");
         instructionstext = GameObject.Find("InstructionText");
-        taptext = GameObject.Find("StartGameText");
+        waitingtext = GameObject.Find("WaitingText");
     }
 
     // Update is called once per frame
@@ -48,25 +76,46 @@ public class TitleScreenScript : MonoBehaviour
     {
         if (state == TitleState.INITIAL)
         {
-            StartCoroutine(timing());
             titletext.GetComponent<Fade>().show(false);
             gameObject.GetComponent<Fade>().show(false);
             instructionstext.GetComponent<Fade>().show(false);
-            taptext.GetComponent<Fade>().show(false);
-            state = TitleState.INSTRUCTIONSIN;
+            waitingtext.GetComponent<Fade>().show(false);
+            state = TitleState.SCREENIN;
             gameObject.GetComponent<Fade>().fadeIn();
             titletext.GetComponent<Fade>().fadeIn();
-        } else if (state == TitleState.INSTRUCTIONSIN && gameObject.GetComponent<Fade>().state == Fade.FadeState.IN) {
-            state = TitleState.INSTRUCTIONS;
-            instructionstext.GetComponent<Fade>().fadeIn();
-        } else if (state == TitleState.INSTRUCTIONSOUTTRIGGER) {
-            state = TitleState.INSTRUCTIONSOUT;
-            instructionstext.GetComponent<Fade>().fadeOut();
-        } else if (state == TitleState.INSTRUCTIONSOUT && instructionstext.GetComponent<Fade>().state == Fade.FadeState.OUT)
+        }
+        else if (state == TitleState.SCREENIN && gameObject.GetComponent<Fade>().state == Fade.FadeState.IN)
         {
-            state = TitleState.TAPIN;
-            taptext.transform.Translate(new Vector3(0, 0.005f, 0));
-            taptext.GetComponent<Fade>().fadeIn();
+            instructionstext.GetComponent<TextMesh>().text = instructions[instructionindex];
+            instructionstext.GetComponent<Fade>().fadeIn();
+            timingcoroutine = timing();
+            StartCoroutine(timingcoroutine);
+            state = TitleState.INSTRUCTIONSIN;
+        }
+        else if (state == TitleState.INSTRUCTIONSIN && instructionstext.GetComponent<Fade>().state == Fade.FadeState.IN)
+        {
+            // wait
+            state = TitleState.INSTRUCTIONS;
+        } else if (state == TitleState.INSTRUCTIONSOUTTRIGGER)
+        {
+            instructionstext.GetComponent<Fade>().fadeOut();
+            state = TitleState.INSTRUCTIONSOUT;
+        }
+        else if (state == TitleState.INSTRUCTIONSOUT && instructionstext.GetComponent<Fade>().state == Fade.FadeState.OUT)
+        {
+            instructionstext.GetComponent<TextMesh>().text = instructions[instructionindex];
+            instructionstext.GetComponent<Fade>().fadeIn();
+            state = TitleState.INSTRUCTIONSIN;
+        } else if (state == TitleState.WAITTRIGGERED)
+        {
+            instructionstext.GetComponent<Fade>().fadeOut();
+            state = TitleState.WAITOUT;
+        }
+        else if (state == TitleState.WAITOUT && instructionstext.GetComponent<Fade>().state == Fade.FadeState.OUT)
+        {
+            state = TitleState.WAITIN;
+            waitingtext.transform.Translate(new Vector3(0, 0.005f, 0));
+            waitingtext.GetComponent<Fade>().fadeIn();
         }
     }
 }
