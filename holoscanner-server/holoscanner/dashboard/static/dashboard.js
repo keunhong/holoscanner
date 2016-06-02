14c61728825ce2f40ef2721f72b76986ff194070
Edward Zhang <edward64@gmail.com>
2016-06-02 15:09:23 -0700

    Change viewer fov

--- holoscanner-server/holoscanner/dashboard/static/dashboard.js
+++ holoscanner-server/holoscanner/dashboard/static/dashboard.js
@@ -287,7 +287,7 @@ function initRenderer() {
   }
 
   let camera = new THREE.PerspectiveCamera(
-      75, container.width() / container.height(), 0.1, 1000);
+      60, container.width() / container.height(), 0.1, 1000);
   camera.position.set(0, 0, -5);
   camera.lookAt(0, 0, 0);
 
